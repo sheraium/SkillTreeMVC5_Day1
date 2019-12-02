3e7effbc623beced7058847642add2f3e3c3ca9b
320 陳永修 <peterch@mirle.com.tw>
2019-12-02 12:44:50 +0800

    Day1 Homework

--- SkillTreeMVC5_Day1/Controllers/HomeController.cs
+++ SkillTreeMVC5_Day1/Controllers/HomeController.cs
@@ -1,7 +1,7 @@
-﻿using System;
+﻿using SkillTreeMVC5_Day1.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace SkillTreeMVC5_Day1.Controllers
@@ -10,7 +10,26 @@ namespace SkillTreeMVC5_Day1.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            var result = new List<MoneyViewModel>();
+            //{
+            //    new MoneyViewModel() {MoneyType = "支出", Date = DateTime.Now, Amount = 300},
+            //    new MoneyViewModel() {MoneyType = "支出", Date = DateTime.Now, Amount = 1600},
+            //    new MoneyViewModel() {MoneyType = "支出", Date = DateTime.Now, Amount = 8.01m},
+            //};
+
+            var random = new Random(Guid.NewGuid().GetHashCode());
+
+            foreach (var i in Enumerable.Range(1, 100))
+            {
+                result.Add(new MoneyViewModel()
+                {
+                    MoneyType = random.NextDouble() > 0.5 ? "支出" : "收入",
+                    Date = DateTime.Now.AddDays(-100 + i),
+                    Amount = random.Next(1, 1000) * 10
+                });
+            }
+
+            return View(result);
         }
 
         public ActionResult About()
---
+++ SkillTreeMVC5_Day1/ViewModels/MoneyViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace SkillTreeMVC5_Day1.ViewModels
+{
+    public class MoneyViewModel
+    {
+        public string MoneyType { get; set; }
+        public DateTime Date { get; set; }
+        public decimal Amount { get; set; }
+    }
+}
