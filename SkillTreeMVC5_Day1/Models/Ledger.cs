1093c498b801e061b11e6f2c1baa11fd14dc3c6f
320 陳永修 <peterch@mirle.com.tw>
2019-12-08 11:12:12 +0800

    add class Ledger

---
+++ SkillTreeMVC5_Day1/Models/Ledger.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SkillTreeMVC5_Day1.Models
+{
+    public class Ledger
+    {
+        public LedgerType Type { get; set; }
+        public DateTime Timestamp { get; set; }
+        public decimal Amount { get; set; }
+        public string Remark { get; set; }
+    }
+
+    public enum LedgerType
+    {
+        Income = 0,
+        Expenses = 1,
+    }
+}
