ea7ad1a4b3b2f5fed971c30f6d93eb6e943148c1
320 陳永修 <peterch@mirle.com.tw>
2019-12-13 20:44:01 +0800

    Extract class: LedgerService

--- SkillTreeMVC5_Day1/Controllers/HomeController.cs
+++ SkillTreeMVC5_Day1/Controllers/HomeController.cs
@@ -1,42 +1,17 @@
-﻿using SkillTreeMVC5_Day1.ViewModels;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
+using SkillTreeMVC5_Day1.Services;
 
 namespace SkillTreeMVC5_Day1.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly LedgerService _ledgerService = new LedgerService();
+
         public ActionResult Index()
         {
-            var result = GetAll();
+            var result = _ledgerService.GetAll();
 
             return View(result);
-        }
-
-        private static List<MoneyViewModel> GetAll()
-        {
-            var result = new List<MoneyViewModel>();
-            //{
-            //    new MoneyViewModel() {MoneyType = "支出", Date = DateTime.Now, Amount = 300},
-            //    new MoneyViewModel() {MoneyType = "支出", Date = DateTime.Now, Amount = 1600},
-            //    new MoneyViewModel() {MoneyType = "支出", Date = DateTime.Now, Amount = 8.01m},
-            //};
-
-            var random = new Random(Guid.NewGuid().GetHashCode());
-
-            foreach (var i in Enumerable.Range(1, 100))
-            {
-                result.Add(new MoneyViewModel()
-                {
-                    MoneyType = random.NextDouble() > 0.5 ? "支出" : "收入",
-                    Date = DateTime.Now.AddDays(-100 + i),
-                    Amount = random.Next(1, 1000) * 10
-                });
-            }
-
-            return result;
         }
 
         public ActionResult About()
---
+++ SkillTreeMVC5_Day1/Services/LedgerService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using SkillTreeMVC5_Day1.ViewModels;
+
+namespace SkillTreeMVC5_Day1.Services
+{
+    public class LedgerService
+    {
+        public List<MoneyViewModel> GetAll()
+        {
+            var result = new List<MoneyViewModel>();
+            //{
+            //    new MoneyViewModel() {MoneyType = "支出", Date = DateTime.Now, Amount = 300},
+            //    new MoneyViewModel() {MoneyType = "支出", Date = DateTime.Now, Amount = 1600},
+            //    new MoneyViewModel() {MoneyType = "支出", Date = DateTime.Now, Amount = 8.01m},
+            //};
+
+            var random = new Random(Guid.NewGuid().GetHashCode());
+
+            foreach (var i in Enumerable.Range(1, 100))
+            {
+                result.Add(new MoneyViewModel()
+                {
+                    MoneyType = random.NextDouble() > 0.5 ? "支出" : "收入",
+                    Date = DateTime.Now.AddDays(-100 + i),
+                    Amount = random.Next(1, 1000) * 10
+                });
+            }
+
+            return result;
+        }
+    }
+}
